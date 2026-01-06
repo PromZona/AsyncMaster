9920698feb8c6acabab3f24270ea4dbd1652fd37
Andrey Essaulov <andrey.essaulov@outlook.com>
2026-01-07 01:09:27 +0200

    refactor: added application struct

--- internal/app/app.go
+++ internal/app/app.go
@@ -12,24 +12,30 @@ import (
 	tele "gopkg.in/telebot.v4"
 )
 
-func Init() (*tele.Bot, *sql.DB, error) {
+type App struct {
+	TeleBot *tele.Bot
+	BotData *bot.BotData
+	DB      *sql.DB
+}
+
+func Init() (*App, error) {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("error loading .env file")
-		return nil, nil, err
+		return nil, err
 	}
 
 	psqlInfo := os.Getenv("DB_CONNECTION_STRING")
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		log.Fatal("failed to open db connection")
-		return nil, nil, err
+		return nil, err
 	}
 
 	err = db.Ping()
 	if err != nil {
 		log.Fatal("failed to ping the server")
-		return nil, nil, err
+		return nil, err
 	}
 	log.Print("Database successfully connected!")
 
@@ -41,15 +47,22 @@ func Init() (*tele.Bot, *sql.DB, error) {
 	b, err := tele.NewBot(pref)
 	if err != nil {
 		log.Fatal("failed to create tele.Bot")
-		return nil, nil, err
+		return nil, err
 	}
 
 	botData := bot.BotInit(db)
 	router.Register(b, botData)
 
-	return b, db, nil
+	app := &App{
+		TeleBot: b,
+		BotData: botData,
+		DB:      db,
+	}
+
+	return app, nil
 }
 
-func Start(b *tele.Bot) {
-	b.Start()
+func (app *App) Start() {
+	defer app.DB.Close()
+	app.TeleBot.Start()
 }
--- main.go
+++ main.go
@@ -3,11 +3,9 @@ package main
 import "github.com/PromZona/AsyncMaster/internal/app"
 
 func main() {
-	b, db, err := app.Init()
+	application, err := app.Init()
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
-
-	app.Start(b)
+	application.Start()
 }
