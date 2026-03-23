e87d84eba92e7496a41cf0cb716ca20622017ac2
Andrey Essaulov <andrey.essaulov@outlook.com>
2026-03-23 15:13:25 +0200

    returned elevate command

--- internal/app/router/router.go
+++ internal/app/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/PromZona/AsyncMaster/internal/app/bot"
+	"github.com/PromZona/AsyncMaster/internal/app/handlers"
 	"github.com/PromZona/AsyncMaster/internal/app/middleware"
 	"github.com/PromZona/AsyncMaster/internal/app/runtime"
 )
@@ -11,8 +12,7 @@ func Register(rt runtime.Runtime, botData *bot.BotData) {
 	rt.Use(middleware.ErrorRecovery(botData))
 	rt.Use(middleware.RegistrationCheck(botData))
 
-	// rt.HandleCommand("/start", func(ctx runtime.Context) error { return registration.StartMessage(ctx) })
-	// rt.HandleCommand("/elevate", func(ctx runtime.Context) error { return master.HandleElevateToMaster(ctx, botData) })
+	rt.HandleCommand("/elevate", func(ctx runtime.Context) error { return handlers.HandleElevateToMaster(ctx, botData) })
 
 	rt.HandleText(func(ctx runtime.Context) error { return DispatchText(ctx, botData) })
 	rt.HandleCallback(func(ctx runtime.Context) error { return DispatchCallback(ctx, botData) })
